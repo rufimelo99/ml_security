bb99976349fa6c947833741de34dd7c068d8d141
Rui Melo <44201826+rufimelo99@users.noreply.github.com>
2024-10-21 23:37:02 +0100

    Adaptative Network - Introduction to Hybrid Linear KANs (#4)
    introducing adaptative linear network

---
+++ ml_security/tests/adaptative_linear_network/test_adaptative_linear_network.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pytest
+import torch
+
+from ml_security.adaptative_linear_network.network import HybridLinearKAN
+
+
+def test_forward():
+    input_size = 28 * 28
+    output_size = 100
+    batch_size = 64
+    input_data = np.random.rand(batch_size, input_size)
+    input_data = torch.tensor(input_data, dtype=torch.float32)
+    model = HybridLinearKAN(input_size, output_size)
+    output = model(input_data)
+    assert output.shape == (batch_size, output_size)
