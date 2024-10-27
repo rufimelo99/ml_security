36d6cfa3c9d0e2d022997222ee6d2888acacea83
Rui Melo <rufimelo99@gmail.com>
2024-10-27 23:37:54 +0000

    adding other init

--- ml_security/ml_security/attacks/__init__.py
+++ ml_security/ml_security/attacks/__init__.py
@@ -1,4 +1,3 @@
-__version__ = "0.0.4"
 from ml_security.attacks.carlini_wagner_attack import CarliniWagnerAttack
 from ml_security.attacks.fast_gradient_sign_attack import FastGradientSignAttack
 from ml_security.attacks.membership_inference_attack import MembershipInferenceAttack
