76707d3e662493346bfa07e55334bbd92e5314d3
Rui Melo <rufimelo99@gmail.com>
2024-10-29 00:52:56 +0000

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,6 +3,9 @@
 # If you wish to continue using _config.yml, make edits to that file and
 # re-generate this one.
 ###############################################################################
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
 author = "Rui Melo"
 project = 'ml_security'
 copyright = '2024, Rui Melo'
